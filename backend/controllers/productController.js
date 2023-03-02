b66a9cc7c58685d73d9d92a3e2251c5ce6645676
Karsyz <mattkars@gmail.com>
2023-03-01 22:28:37 -0500

    connected database, created product schema and updated controllers to interact with db

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,17 +1,26 @@
 const asyncHandler = require('express-async-handler')
 
+const Product = require('../models/productModel')
+
 //  @desc   Get all products
 //  @route  GET /api/products
 //  @access Public
 const getAllProducts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get all Products' })
+  const products = await Product.find() 
+
+  res.status(200).json(products)
 })
 
 //  @desc   Get one product
 //  @route  GET /api/products/:id
 //  @access Public
 const getProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get product ${req.params.id}`})
+  const product = await Product.findById(req.params.id)
+  if(!product) {
+    res.status(400)
+    throw new Error('Product not found')
+  }
+  res.status(200).json(product)
 })
 
 
@@ -22,10 +31,11 @@ const createProduct = asyncHandler(async (req, res) => {
   if(!req.body.name) {
     res.status(400)
     throw new Error('Please add a name field')
-  }else { 
   }
-  
-  res.status(200).json({ message: 'Create a Product' })
+  const product = await Product.create({
+    name: req.body.name
+  })
+  res.status(200).json(product)
 })
 
 
@@ -33,7 +43,13 @@ const createProduct = asyncHandler(async (req, res) => {
 //  @route  PUT /api/products/:id
 //  @access Private
 const updateProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update product ${req.params.id}`})
+  const product = await Product.findById(req.params.id)
+  if(!product) {
+    res.status(400)
+    throw new Error('Product not found')
+  }
+  const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, })
+  res.status(200).json(updatedProduct)
 })
 
 
@@ -41,7 +57,17 @@ const updateProduct = asyncHandler(async (req, res) => {
 //  @route  GET /api/product/:id
 //  @access Private
 const deleteProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete product ${req.params.id}`})
+  const product = await Product.findById(req.params.id)
+  console.log(product)
+
+  if(!product) {
+    res.status(400)
+    throw new Error('Product not found')
+  }
+
+  await product.deleteOne()
+
+  res.status(200).json({ id: req.params.id })
 })
 
 
